15467f094cc6bc720b8cba45958f7129458945f3
Christoffer Lilja <christoffer.lilja@gmail.com>
2013-04-07 22:56:46 +0200

    Using --evall instead of .js files
    Signed-off-by: Christoffer Lilja <christoffer.lilja@gmail.com>

--- close_mongod.js
+++
@@ -1,2 +0,0 @@
-use admin;
-db.shutdownServer();
--- init_shard_1.js
+++
@@ -1,6 +0,0 @@
-config = { _id : "s1", members : [
-{ _id : 0, host : "localhost:47017"},
-{ _id : 1, host : "localhost:47018" },
-{ _id : 2, host : "localhost:47019" } ]
-}
-rs.initiate(config)
